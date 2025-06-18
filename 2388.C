81e3db0fc10279abdbdfa196ece4ca7bbb6df9af
Victoria <107559895+Victoriaaahh@users.noreply.github.com>
2025-06-18 11:36:57 -0300

    Create 2388.C

---
+++ 2388.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int N, T, V;
+    int total = 0;
+    
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d", &T, &V);
+        total += T * V;
+    }
+
+    printf("%d\n", total);
+
+    return 0;
+}
